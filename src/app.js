6df0e57b4d9c5b0111a58b7fa5eb998b3602edd8
Thomas Lautaro López <laautarolopez@gmail.com>
2024-07-18 20:36:02 -0300

    se crea una seed para cargar las tablas de la db

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require("express")
 const usersRoutes = require('./routes/users.routes.js')
 const creditsRoutes = require('./routes/credits.routes.js')
 const swaggerDocs = require('./swagger/swagger.js')
+const { runSeed } = require("./db/seed.js")
 
 const app = express()
 
 app.use(express.json())
+runSeed()
 
 app.use('/api', usersRoutes)
 app.use('/api', creditsRoutes)
---
+++ src/db/seed.js
@@ -0,0 +1,35 @@
+const { query, end } = require('./db.js')
+
+const runSeed = async () => {
+    try {
+        // Crear la tabla "users" si no existe
+        await query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id TEXT PRIMARY KEY,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                credits NUMERIC DEFAULT 0
+            );
+        `)
+        console.log(`Created "users" table`)
+
+        // Crear la tabla "transfers" si no existe
+        await query(`
+            CREATE TABLE IF NOT EXISTS transfers (
+                id TEXT PRIMARY KEY,
+                from_id TEXT REFERENCES users(id),
+                to_id TEXT REFERENCES users(id),
+                amount NUMERIC NOT NULL,
+                arrival_date TIMESTAMP NOT NULL
+            );
+        `)
+        console.log(`Created "transfers" table`)
+
+        await end()
+    } catch (error) {
+      console.error('Error seeding:', error)
+      throw error
+    }
+}
+
+module.exports = { runSeed }
